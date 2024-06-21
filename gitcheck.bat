2727791703e8dc7b07e4edeff7472299d1b2eb91
nuchad <nuanuchad@gmail.com>
2024-06-21 21:53:26 +0700

    add file .bat for checkVersion release

---
+++ gitcheck.bat
@@ -0,0 +1,29 @@
+@echo off
+setlocal enabledelayedexpansion
+
+echo Start check vertion release tag
+cd "C:\Users\Anuch\OneDrive\project_microservice"
+
+rem ดึงข้อมูล tag ปัจจุบัน
+for /f "delims=" %%a in ('git describe --tags') do (
+    set "current_tag=%%a"
+)
+
+rem ดึงข้อมูล tag ล่าสุดจาก git
+for /f "delims=" %%i in ('git ls-remote --tags %gitRemote%') do set latest_tag=%%i
+for /f "tokens=2" %%a in ("%latest_tag%") do (
+    set tag=%%a
+    set latest_tag=!tag:~10!
+)
+
+echo current_tag : %current_tag%
+echo latest_tag : %latest_tag%
+
+if "%current_tag%" neq "%latest_tag%" (
+    echo Pulling latest changes...
+    git pull
+) else (
+    echo No new changes to pull.
+)
+
+echo End script
